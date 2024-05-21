855c4dbfbdeb408475d781a14499f0b2feac0837
Mateus Bessa <mateusbessacontato@gmail.com>
2024-05-21 13:50:34 -0300

    v.0.0.1

--- src/pages/Alfabeto.jsx
+++ src/pages/Alfabeto.jsx
@@ -14,8 +14,9 @@ const Alfabeto = () => {
     const handleImagem = () => {
         const indice = Math.round(Math.random() * (images.length - 1))
         setLetter({ ...letter, revealed: false })
-        setMessage("hidden")
         setImage(images[indice])
+        setMessage("hidden")
+        setAnswer("")
     }
 
     const handleSubmit = (e) => {
@@ -23,6 +24,9 @@ const Alfabeto = () => {
         if (answer.toLowerCase() === letter.value.toLowerCase()) {
             setLetter({ ...letter, revealed: true })
             setMessage("Você acertou!")
+            setTimeout(() => {
+                handleImagem()
+            }, 1000)
         } else {
             setMessage("Resposta errada!")
             setTimeout(() => {
@@ -33,39 +37,46 @@ const Alfabeto = () => {
 
     return (
         <main>
-            <div className="container">
-                <h2 className={`${letter.revealed ? "revealed" : "hidden"}`}>Resposta: {letter.value}</h2>
-                <img src={image} alt="Letra do alfabeto grego" />
-                <h2 className={`${message === "hidden" ? "hidden" : "revealed"}`}>{message}</h2>
+
+            <div className="card">
+                <h1 className="text-center mb-2 text-[40px] max-sm:text-4xl">Adivinhe a letra</h1>
+                <h2 className="text-slate-500">Greek Alphabet</h2>
+                <h2 className={`${letter.revealed ? "revealed" : "opacity-0"} text-2xl`}>Resposta: {letter.value}</h2>
+                <img className="w-[400px] max-sm:w-[250px] h-[200px] mt-5" src={image} alt="Letra do alfabeto grego" />
+                <h2 className={`${message === "hidden" ? "opacity-0" : "revealed"} text-2xl my-4`}>{message}</h2>
                 <form>
                     <input
-                        className="input-estilizado"
                         onChange={(e) => setAnswer(e.target.value)}
                         placeholder="Digite aqui o nome da letra"
+                        className="input p-4 h-[40px] max-sm:w-[220px]"
+                        value={answer}
                         type="text"
                     />
                     <button
-                        className="btn"
                         onClick={handleSubmit}
+                        className="btn h-[40px] w-[100px] max-sm:w-[75px]"
                     >
                         Enviar
                     </button>
                 </form>
-                <div className="button-container">
+                <div className="flex gap-3 justify-center items-center mt-5">
                     <button
-                        className="btn"
                         onClick={(e) => setLetter({ ...letter, revealed: true })}
+                        className="btn w-[170px] max-sm:w-[140px] h-[40px]"
                     >
                         Revele a letra
                     </button>
                     <button
-                        className="btn"
                         onClick={handleImagem}
+                        className="btn w-[170px] max-sm:w-[140px] h-[40px]"
                     >
                         Altere a imagem
                     </button>
                 </div>
             </div>
+            <footer>
+                <p className="ml-1 mt-1">Developed by: Mateus Bessa</p>
+            </footer>
         </main>
     )
 }
